10841e83c7d7453902e393a70aebfb5ac635aedf
Florin iuonas <iuonasc@yahoo.com>
2025-04-27 17:15:53 +0300

    phase 2'

--- proiect1.c
+++ proiect1.c
@@ -1,4 +1,4 @@
-/* treasure_manager.c - Phase 1 implementation */
+/* treasure_system.c - Phase 1 + Phase 2 implementation */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,12 +7,17 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
 #include <time.h>
 
 #define TREASURE_FILE "treasures.dat"
 #define LOG_FILE "logged_hunt"
+#define COMMAND_FILE "command.txt"
 #define MAX_CLUE 256
 #define USER_NAME_SIZE 32
+
+// Phase 1 - Treasure Management
 
 typedef struct {
     int treasure_id;
@@ -39,6 +44,8 @@ void create_hunt_dir(char *hunt_id) {
         perror("mkdir");
         exit(1);
     }
+}
+
 void add_treasure(char *hunt_id) {
     Treasure tr;
     char path[256];
@@ -117,22 +124,131 @@ void remove_hunt(char *hunt_id) {
     system(cmd);
 }
 
-int main(int argc, char **argv) {
-    if(argc < 3) { printf("Usage error\n"); exit(1); }
+// Phase 2 - Processes & Signals
 
-    if(!strcmp(argv[1], "--add")) add_treasure(argv[2]);
-    else if(!strcmp(argv[1], "--list")) list_treasures(argv[2]);
-    else if(!strcmp(argv[1], "--remove_treasure")) {
-        if(argc != 4) { printf("Usage error\n"); exit(1); }
-        remove_treasure(argv[2], atoi(argv[3]));
+pid_t monitor_pid = -1;
+int monitor_exiting = 0;
+
+void sigchld_handler(int sig) {
+    wait(NULL);
+    monitor_pid = -1;
+    printf("Monitor has exited.\n");
+}
+
+void monitor_process() {
+    struct sigaction sa;
+    sa.sa_handler = SIG_IGN;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
+    sigaction(SIGINT, &sa, NULL);
+
+    while(1) {
+        pause();
+
+        FILE *cmd = fopen(COMMAND_FILE, "r");
+        if (!cmd) continue;
+
+        char command[32];
+        char hunt_id[128];
+        int tid;
+        fscanf(cmd, "%s", command);
+
+        if (strcmp(command, "list_hunts") == 0) {
+            system("ls -d */ | sed 's#/##'");
+        }
+        else if (strcmp(command, "list_treasures") == 0) {
+            fscanf(cmd, "%s", hunt_id);
+            list_treasures(hunt_id);
+        }
+        else if (strcmp(command, "view_treasure") == 0) {
+            fscanf(cmd, "%s %d", hunt_id, &tid);
+            printf("View Treasure ID: %d in Hunt: %s (details not implemented yet)\n", tid, hunt_id);
+        }
+        else if (strcmp(command, "stop") == 0) {
+            printf("Monitor exiting...\n");
+            usleep(2000000); // 2 seconds delay
+            exit(0);
+        }
+        fclose(cmd);
     }
-    else if(!strcmp(argv[1], "--remove_hunt")) remove_hunt(argv[2]);
-    else printf("Unknown command\n");
+}
 
-    char symlink_path[256], logfile_path[256];
-    snprintf(logfile_path, sizeof(logfile_path), "%s/%s", argv[2], LOG_FILE);
-    snprintf(symlink_path, sizeof(symlink_path), "logged_hunt-%s", argv[2]);
-    //symlink(logfile_path, symlink_path);
+void start_monitor() {
+    monitor_pid = fork();
+    if (monitor_pid == 0) {
+        monitor_process();
+        exit(0);
+    } else if (monitor_pid < 0) {
+        perror("fork");
+        exit(1);
+    }
+    printf("Monitor started. PID: %d\n", monitor_pid);
+}
 
+void send_command(const char *cmdline) {
+    FILE *cmd = fopen(COMMAND_FILE, "w");
+    if (!cmd) { perror("fopen"); return; }
+    fprintf(cmd, "%s\n", cmdline);
+    fclose(cmd);
+}
+
+int main(int argc, char **argv) {
+    struct sigaction sa;
+    sa.sa_handler = sigchld_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    sigaction(SIGCHLD, &sa, NULL);
+
+    while (1) {
+        char input[256];
+        printf("Enter command: ");
+        if (!fgets(input, sizeof(input), stdin)) break;
+        strtok(input, "\n");
+
+        if (strcmp(input, "start_monitor") == 0) {
+            if (monitor_pid > 0) printf("Monitor already running.\n");
+            else start_monitor();
+        }
+        else if (strcmp(input, "list_hunts") == 0) {
+            if (monitor_pid > 0) {
+                send_command("list_hunts");
+                kill(monitor_pid, SIGUSR1);
+            } else printf("Monitor not running.\n");
+        }
+        else if (strncmp(input, "list_treasures", 14) == 0) {
+            if (monitor_pid > 0) {
+                char *hunt = input + 15;
+                char buffer[256];
+                snprintf(buffer, sizeof(buffer), "list_treasures %s", hunt);
+                send_command(buffer);
+                kill(monitor_pid, SIGUSR2);
+            } else printf("Monitor not running.\n");
+        }
+        else if (strncmp(input, "view_treasure", 13) == 0) {
+            if (monitor_pid > 0) {
+                char *args = input + 14;
+                send_command(args);
+                kill(monitor_pid, SIGTERM);
+            } else printf("Monitor not running.\n");
+        }
+        else if (strcmp(input, "stop_monitor") == 0) {
+            if (monitor_pid > 0) {
+                send_command("stop");
+                kill(monitor_pid, SIGINT);
+                monitor_exiting = 1;
+            } else printf("Monitor not running.\n");
+        }
+        else if (strcmp(input, "exit") == 0) {
+            if (monitor_pid > 0) printf("Cannot exit while monitor is running!\n");
+            else break;
+        }
+        else {
+            printf("Unknown command.\n");
+        }
+    }
     return 0;
 }
